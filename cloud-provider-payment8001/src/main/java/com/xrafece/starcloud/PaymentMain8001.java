3f8470a8f01aef49033b23f091ba921cce5a8d85
Xrafece <286402807@qq.com>
2020-12-12 21:50:09 +0800

    add cloud-payment-service to eureka

--- cloud-provider-payment8001/src/main/java/com/xrafece/starcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/xrafece/starcloud/PaymentMain8001.java
@@ -2,11 +2,13 @@ package com.xrafece.starcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Xrafece
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
