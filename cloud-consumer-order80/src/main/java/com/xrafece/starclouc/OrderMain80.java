a0c21bf736c35060073a55edb40d675234476c21
Xrafece <286402807@qq.com>
2020-12-12 22:24:10 +0800

    add cloud-order-service to eureka and test microservice

--- cloud-consumer-order80/src/main/java/com/xrafece/starclouc/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/xrafece/starclouc/OrderMain80.java
@@ -2,11 +2,13 @@ package com.xrafece.starclouc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Xrafece
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
