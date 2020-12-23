00a13bca7cac33d7ef8af0f9e04baf05a494aa6f
Xrafece <286402807@qq.com>
2020-12-23 15:43:05 +0800

    project add cloud-gateway-gateway9527 module

---
+++ cloud-gateway-gateway9527/src/main/java/com/xrafece/starcloud/GateWayMain9527.java
@@ -0,0 +1,16 @@
+package com.xrafece.starcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Xrafece
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
